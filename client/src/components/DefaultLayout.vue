d88e6fea055f4a81f808842443106c31a3aadcb7
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-06-07 17:42:19 +0100

    add logout

--- client/src/components/DefaultLayout.vue
+++ client/src/components/DefaultLayout.vue
@@ -39,8 +39,13 @@
                 </div>
                 <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                   <MenuItems class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                      <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                    <MenuItem v-slot="{}">
+                      <a
+                        @click="logout"
+                        :class="['block px-4 py-2 text-sm text-gray-700 cursor-pointer'
+                        ]">
+                          Sign out
+                      </a>
                     </MenuItem>
                   </MenuItems>
                 </transition>
@@ -71,10 +76,6 @@
               <div class="text-base font-medium leading-none text-white"></div>
               <div class="text-sm font-medium leading-none text-gray-400">{{ user.name }}</div>
             </div>
-            <button type="button" class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
           </div>
           
         </div>
@@ -89,16 +90,13 @@ import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuIt
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 import { useStore } from "vuex";
 import { computed } from "vue";
+import  { useRouter } from "vue-router";
 
 
 const navigation = [
-  { name: 'Products', to: { name: 'Products' }},
-  { name: 'About Us', to: { name: 'About' }},
-  { name: 'Contact', to: { name: 'Contact' }},
+  // { name: 'Products', to: { name: 'Products' }},
 ]
-const userNavigation = [
-  { name: 'Sign out', href: '#' },
-];
+
 
 export default {
   components: {
@@ -117,11 +115,17 @@ export default {
   setup() {
 
     const store = useStore();
+    const router = useRouter();
+
+    function logout() {
+      store.commit('logout')
+      router.push({ name: 'Login' })
+    };
 
     return {
       user: computed(() => store.state.user.data),
       navigation,
-      userNavigation,
+      logout
     };
   }
 }
