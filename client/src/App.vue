acd751af5ed3ce09c9a322ffbd3568badf9e2cc3
abdelhaq-97 <nouhiabdelhak5@gmail.com>
2023-06-07 15:33:47 +0100

    added register and login interface

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
 
 </script>
 
 <template>
-  <div>
-    <HelloWorld msg="Hello Vue 3 + Vite" />
-  </div>
+  <router-view></router-view>
 </template>
 
 <style scoped>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,18 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import DefaultLayout from '../components/DefaultLayout.vue'
+import store from '../store/index.js'
+import Products from '../views/Products.vue'
 
 const routes = [
     {
         path: '/',
-        name: 'Dashboard',
-        component: Dashboard,
+        redirect: '/Products',
+        name: 'Products',
+        component: DefaultLayout,
+        meta: { requiresAuth: true },
+        children: [
+            { path: '/Products', name: 'Products', component: Products, },
+        ]
     },
     {
-        path: '/login',
+        path: '/Login',
         name: 'Login',
         component: Login,
     },
     {
-        path: '/register',
+        path: '/Register',
         name: 'Register',
         component: Register,
     }
@@ -21,6 +31,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if(to.meta.requiresAuth && !store.state.user.token){
+        next({name: 'Login'})
+    }else if(store.state.user.token && (to.name === 'Login' || to.name === 'Register')){
+        next({ name: 'Products'})
+    } else {
+        next()
+    }
 })
 
 export default router
